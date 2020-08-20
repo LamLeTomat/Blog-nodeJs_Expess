6f64bc9861d99eb92b19d4ce74cc4a1fc4a25e3e
LamLeTomat <lamletomat@ubuntu.ubuntu-domain>
2020-08-21 02:12:47 +0700

    Update course, add me/stored/courses

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
+
 const route = require('./routes');
-const db = require('./config/db')
+const db = require('./config/db');
 
 //connect to db
 db.connect();
@@ -18,6 +20,8 @@ app.listen(port, () => console.log(` app listening at http://localhost:${port}`)
 //HTTP logger
 // app.use(morgan('combined'));
 
+app.use(methodOverride('_method'));
+
 //set up static file is public folder
 app.use(express.static(path.join(__dirname,'public')));
 //Use midde ware for Post method
@@ -29,7 +33,10 @@ app.use(express.json());
 //Template engine
 app.engine('hbs', handlebars(
     {
-        extname: '.hbs'
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a+b,
+        }
     }
 ));
 app.set('view engine', 'hbs');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const newsRouter = require('./news.route');
 const siteRouter = require('./site');
 const coursesRouter = require('./courses');
+const meRouter = require('./me');
 
 function route(app) {
     
     app.use('/news', newsRouter);
     app.use('/courses',coursesRouter);
     app.use('/', siteRouter);
+    app.use('/me',meRouter);
     
 };
 module.exports = route;
